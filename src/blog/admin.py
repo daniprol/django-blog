87dadc39e009a3268cee519d68f4bf407f49469d
Dani Prol <prolcambados@gmail.com>
2025-01-24 00:12:43 +0100

    customize Post model in admin

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "publish", "status"]
+    list_filter = ["status", "created", "publish", "author"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"slug": ("title",)}
+    # Allows to search for user name when creating a post, instead of having a dropdown
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish"  # navigation on top of the table
+    ordering = ["status", "publish"]
+
+    # Will always show "counts". Instead of being toggable
+    show_facets = admin.ShowFacets.ALWAYS
