1fd0b4b73ee02bd32e5c077f4ae2df521c5fc552
Dani Prol <prolcambados@gmail.com>
2025-01-25 11:06:28 +0100

    fix pagination error

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import Http404
 from django.http.request import HttpRequest
 from django.shortcuts import get_object_or_404, render
@@ -11,8 +11,13 @@ def post_list(request: HttpRequest):
 
     paginator = Paginator(post_list, 3)  # 3 posts per page
     page_number = request.GET.get("page", 1)
-    posts = paginator.page(page_number)
-    # "posts" is an object of type Page
+    try:
+        posts = paginator.page(page_number)
+        # "posts" is an object of type Page
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     return render(request, "blog/post/list.html", {"posts": posts})
 
